af4fe14031594798e143501be51f6fa19ffb4745
imenk1986 <31105782+imenk1986@users.noreply.github.com>
2022-11-18 15:39:23 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Bonjour a tous !");
+        System.out.println("Bonjour a tous nnnnn!");
   
     }
 }
